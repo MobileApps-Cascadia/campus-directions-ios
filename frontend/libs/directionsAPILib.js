57cd74b1342f5b434c66d9551d9bff2ecb6524d6
James Hayes <jchayes86@gmail.com>
2020-05-21 17:48:49 -0700

    Pod update

--- frontend/libs/directionsAPILib.js
+++ frontend/libs/directionsAPILib.js
@@ -4,14 +4,14 @@ import MapboxGL from "@react-native-mapbox-gl/maps";
 MapboxGL.setAccessToken(config.mapBox.ACCESS_TOKEN);
 
 // create a function to make a directions request
-function getRoute(start, end) {
+export async function getRoute(start, end) {
   // make a directions request using walking profile
   var url = 'https://api.mapbox.com/directions/v5/mapbox/walking/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
 
   // make an XHR request https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
   var req = new XMLHttpRequest();
   req.open('GET', url, true);
-  req.onload = function () {
+  req.onload = await function () {
     var json = JSON.parse(req.response);
     var data = json.routes[0];
     var route = data.geometry.coordinates;
@@ -23,36 +23,41 @@ function getRoute(start, end) {
         coordinates: route
       }
     };
-    // if the route already exists on the map, reset it using setData
-    if (map.getSource('route')) {
-      map.getSource('route').setData(geojson);
-    } else { // otherwise, make a new request
-      map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'LineString',
-              coordinates: geojson
-            }
-          }
-        },
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#3887be',
-          'line-width': 5,
-          'line-opacity': 0.75
-        }
-      });
-    }
+    // // if the route already exists on the map, reset it using setData
+    // if (map.getSource('route')) {
+    //   map.getSource('route').setData(geojson);
+    // } else { // otherwise, make a new request
+    //   map.addLayer({
+    //     id: 'route',
+    //     type: 'line',
+    //     source: {
+    //       type: 'geojson',
+    //       data: {
+    //         type: 'Feature',
+    //         properties: {},
+    //         geometry: {
+    //           type: 'LineString',
+    //           coordinates: geojson
+    //         }
+    //       }
+    //     },
+    //     layout: {
+    //       'line-join': 'round',
+    //       'line-cap': 'round'
+    //     },
+    //     paint: {
+    //       'line-color': '#3887be',
+    //       'line-width': 5,
+    //       'line-opacity': 0.75
+    //     }
+    //   });
+    // }
     // add turn instructions here at the end
   };
   req.send();
+
+  var steps = data.legs[0].steps;
+
+  var tripInstructions = [];
+  console.log(data[0]);
 }
