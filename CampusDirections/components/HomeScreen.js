a1351c27478a9a8c994db7e3f27885060690fe97
James Hayes <jchayes86@gmail.com>
2020-05-26 20:24:11 -0700

    asset updates

--- CampusDirections/components/HomeScreen.js
+++ CampusDirections/components/HomeScreen.js
@@ -14,6 +14,7 @@ import {
 
 import Colors from '../styles/Colors';
 import { Header } from './Header';
+// import { Header } from 'react-native/Libraries/NewAppScreen';
 
 import config from '../config';
 import { getDirections } from '../libs/directionsAPILib';
